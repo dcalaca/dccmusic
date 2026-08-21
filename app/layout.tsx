edffa843e672b46a8f816ce25d730181867ddea7
dcalaca <dcalaca@gmail.com>
2026-08-21 15:52:27 -0300

    Instala Microsoft Clarity na DCC Music (#23)
    Adiciona o projeto y5zbvxwvh2 ao layout global com carregamento afterInteractive.

--- app/layout.tsx
+++ app/layout.tsx
@@ -215,6 +215,16 @@ export default function RootLayout({
           src="https://www.googletagmanager.com/gtag/js?id=G-CNBQFWQ9QT"
           strategy="afterInteractive"
         />
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "y5zbvxwvh2");
+          `}
+        </Script>
         {/* Meta Pixel */}
         <Script id="meta-pixel" strategy="afterInteractive">
           {`
