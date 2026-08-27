28e91aae04fc03a27a1cb14e0d1cf314af6d3b56
dcalaca <dcalaca@gmail.com>
2026-08-26 22:51:40 -0300

    Enable friendly Studio audio download names

--- app/compositores/admin/studio-ia/layout.tsx
+++ app/compositores/admin/studio-ia/layout.tsx
@@ -1,10 +1,12 @@
 import AutoMusicTitleHelper from './AutoMusicTitleHelper'
+import FriendlyAudioDownloads from './FriendlyAudioDownloads'
 
 export default function StudioIaAdminLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       {children}
       <AutoMusicTitleHelper />
+      <FriendlyAudioDownloads />
     </>
   )
 }
