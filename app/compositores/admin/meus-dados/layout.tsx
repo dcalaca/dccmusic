44915afa2f8356debf9e0fa987e67736062bcbdc
dcalaca <dcalaca@gmail.com>
2026-09-01 21:00:54 -0300

    perf: simplify composer account settings

--- app/compositores/admin/meus-dados/layout.tsx
+++ app/compositores/admin/meus-dados/layout.tsx
@@ -1,17 +1,7 @@
 import type { ReactNode } from 'react'
-import ComposerPublicNameEditor from '@/components/ComposerPublicNameEditor'
-import PremiumDirectoryVisibilityCard from './PremiumDirectoryVisibilityCard'
 
 export default function ComposerMyDataLayout({ children }: { children: ReactNode }) {
   return (
-    <>
-      {children}
-      <ComposerPublicNameEditor />
-      <div className="container mx-auto px-4 pb-7 sm:px-6 lg:px-8">
-        <div className="mx-auto max-w-6xl">
-          <PremiumDirectoryVisibilityCard />
-        </div>
-      </div>
-    </>
+    <>{children}</>
   )
 }
