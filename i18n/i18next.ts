f77d5f115e7c91068228debc0d877ccdb0ecd3c6
dcalaca <dcalaca@gmail.com>
2026-09-22 14:05:02 -0300

    chore(i18n): prepare i18next integration

---
+++ i18n/i18next.ts
@@ -0,0 +1,52 @@
+import { createInstance, type i18n, type InitOptions } from 'i18next'
+import { initReactI18next } from 'react-i18next'
+
+import { defaultLocale, normalizeLocale, type AppLocale } from './config'
+
+import ptBR from './messages/pt-BR.json'
+import ptPT from './messages/pt-PT.json'
+import enUS from './messages/en-US.json'
+import enGB from './messages/en-GB.json'
+import esES from './messages/es-ES.json'
+import esMX from './messages/es-MX.json'
+import esCO from './messages/es-CO.json'
+import esPY from './messages/es-PY.json'
+
+export const translationNamespace = 'translation' as const
+
+export const i18nResources = {
+  'pt-BR': { [translationNamespace]: ptBR },
+  'pt-PT': { [translationNamespace]: ptPT },
+  'en-US': { [translationNamespace]: enUS },
+  'en-GB': { [translationNamespace]: enGB },
+  'es-ES': { [translationNamespace]: esES },
+  'es-MX': { [translationNamespace]: esMX },
+  'es-CO': { [translationNamespace]: esCO },
+  'es-PY': { [translationNamespace]: esPY },
+} as const
+
+export function getI18nOptions(locale?: string | null): InitOptions {
+  const normalizedLocale = normalizeLocale(locale)
+
+  return {
+    resources: i18nResources,
+    lng: normalizedLocale,
+    fallbackLng: defaultLocale,
+    supportedLngs: Object.keys(i18nResources),
+    defaultNS: translationNamespace,
+    ns: [translationNamespace],
+    interpolation: {
+      escapeValue: false,
+    },
+    returnNull: false,
+  }
+}
+
+export async function createDccI18n(locale: AppLocale | string = defaultLocale): Promise<i18n> {
+  const instance = createInstance()
+
+  instance.use(initReactI18next)
+  await instance.init(getI18nOptions(locale))
+
+  return instance
+}
--- i18n/index.ts
+++ i18n/index.ts
@@ -8,4 +8,11 @@ export {
   type AppLocale,
 } from './config'
 
+export {
+  createDccI18n,
+  getI18nOptions,
+  i18nResources,
+  translationNamespace,
+} from './i18next'
+
 export { loadMessages, type Messages } from './messages'
